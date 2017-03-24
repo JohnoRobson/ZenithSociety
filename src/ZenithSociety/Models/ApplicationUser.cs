761b6e7ff182b82d210a2d72d095241b99c34fbb
johnorobson <johnathan.g.robson@gmail.com>
2017-03-24 14:05:04 -0700

    validation and error messages are done

--- src/ZenithSociety/Models/ApplicationUser.cs
+++ src/ZenithSociety/Models/ApplicationUser.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace ZenithSociety.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
+
+        [Required]
+
+        [Required]
     }
 }
