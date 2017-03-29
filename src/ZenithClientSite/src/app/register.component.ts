b89dadfcc3f34fbbe3646778d0e45b80cb82cdc8
johnorobson <johnathan.g.robson@gmail.com>
2017-03-29 11:24:19 -0700

    Added in some stuff for registration and connected the angular side to the deployed website

--- src/ZenithClientSite/src/app/register.component.ts
+++ src/ZenithClientSite/src/app/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { TokenService } from './token.service';
 
 @Component({
   selector: 'register',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
 })
 
 export class RegisterComponent {
-    
+    private URL = 'http://zenith-society-nj-core.azurewebsites.net/Account/Register';
+
+    constructor(private http: Http, private tokenService: TokenService) {}
+
+    username: string;
+    password: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+
+    onSubmit() { this.register(this.username, this.password, this.email, this.firstname, this.lastname) }
+    register(username: string, password: string, email: string, firstname: string, lastname: string) {
+      let creds = 'username='+username+'&password='+password+'&confirmpassword='+password+'&email='+email+'&firstname='+firstname+'&lastname='+lastname;
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(this.URL, creds, options)
+            .toPromise()
+            .then(r => {
+                
+                let user = r.json();
+                this.tokenService.setToken(user["access_token"]);
+            });
+    }
 }
