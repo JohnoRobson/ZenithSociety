b273cff0b340072fe01ca73ca49bc02db15f488e
Niko <nlauron@telus.net>
2017-04-02 16:39:24 -0700

    Hid links according to role and deleted extra pages

--- src/ZenithWebSite/Controllers/ActivitiesController.cs
+++ src/ZenithWebSite/Controllers/ActivitiesController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ZenithSociety.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize]
     public class ActivitiesController : Controller
     {
         private readonly ApplicationDbContext _context;
